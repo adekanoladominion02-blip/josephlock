e2f2d69a6bc0ec8f33b962e17b1621dcab33c49e
adekanoladominion02-blip <adekanoladominion02@gmail.com>
2026-02-25 00:14:11 +0100

    Add color palette for design system

---
+++ lib/design_system/colors.dart
@@ -0,0 +1,7 @@
+// Color Palette
+class AppColors {
+  static const Color primary = Color(0xFF0A74DA);
+  static const Color secondary = Color(0xFFFF5733);
+  static const Color background = Color(0xFFF4F4F4);
+  static const Color text = Color(0xFF242424);
+}
